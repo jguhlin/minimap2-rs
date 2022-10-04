603652bd68cb38541885584de551ad1f3ebb5c8b
Joseph Guhlin <joseph.guhlin@gmail.com>
2022-10-04 16:24:52 +1300

    Working on indexing

--- build.rs
+++ build.rs
@@ -3,13 +3,12 @@ use std::path::PathBuf;
 
 #[cfg(feature = "bindgen")]
 fn gen() {
-
     println!("cargo:rerun-if-changed=minimap2/*.c");
     println!("cargo:rustc-link-lib=static=libminimap");
 
-    // kthread.o kalloc.o misc.o bseq.o sketch.o sdust.o options.o index.o 
-    // lchain.o align.o hit.o seed.o map.o format.o pe.o esterr.o splitidx.o 
-    // ksw2_ll_sse.o ksw2_extz2_sse41.o ksw2_extd2_sse41.o ksw2_exts2_sse41.o 
+    // kthread.o kalloc.o misc.o bseq.o sketch.o sdust.o options.o index.o
+    // lchain.o align.o hit.o seed.o map.o format.o pe.o esterr.o splitidx.o
+    // ksw2_ll_sse.o ksw2_extz2_sse41.o ksw2_extd2_sse41.o ksw2_exts2_sse41.o
     // ksw2_extz2_sse2.o ksw2_extd2_sse2.o ksw2_exts2_sse2.o ksw2_dispatch.o
 
     let mut cc = cc::Build::new();
@@ -23,13 +22,16 @@ fn gen() {
     cc.flag("-DKSW_CPU_DISPATCH");
     cc.flag("-DKSW_SSE2_ONLY");
 
-    let files: Vec<_> = std::fs::read_dir("minimap2").unwrap().map(|f| f.unwrap().path()).collect();
+    let files: Vec<_> = std::fs::read_dir("minimap2")
+        .unwrap()
+        .map(|f| f.unwrap().path())
+        .collect();
     for file in files {
         // Skip "main.c" and "example.c"
         if file.file_name().unwrap() == "main.c" || file.file_name().unwrap() == "example.c" {
             continue;
         }
-        
+
         if let Some(x) = file.extension() {
             if x == "c" {
                 cc.file(file);
@@ -41,7 +43,7 @@ fn gen() {
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let bindings = bindgen::Builder::default()
+    bindgen::Builder::default()
         .header("minimap2.h")
         .header("kseq.h")
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
@@ -69,19 +71,21 @@ fn gen() {
         .allowlist_function("kseq_read")
         .allowlist_function("kseq_destroy")
         .allowlist_function("mm_map")
+        .allowlist_function("mm_idx_str")
         .generate()
         .expect("Couldn't write bindings!")
-        .write_to_file(out_path.join("bindings.rs"));
-    
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Unable to create bindings");
 }
 
 #[cfg(not(feature = "bindgen"))]
 fn gen() {}
 
 fn main() {
-    // println!("cargo:rustc-link-lib=z");
-    // println!("cargo:rustc-link-lib=pthread");
-    // println!("cargo:rustc-link-lib=m");
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rustc-link-lib=z");
+    println!("cargo:rustc-link-lib=pthread");
+    println!("cargo:rustc-link-lib=m");
     println!("cargo:rustc-link-lib=static=libminimap");
     gen();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,111 +5,267 @@
 #[cfg(feature = "bindgen")]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+use std::error::Error;
+use std::mem::MaybeUninit;
+use std::path::Path;
+
 #[cfg(not(feature = "bindgen"))]
 pub mod bindings;
 
 #[cfg(not(feature = "bindgen"))]
 pub use bindings::*;
 
-pub static PRESET0: i8 = 0;
-/*
+pub static MAP_ONT: &str = "map-ont\0";
+pub static AVA_ONT: &str = "ava-ont\0";
+pub static MAP10K: &str = "map10k\0";
+pub static AVA_PB: &str = "ava-pb\0";
+pub static MAP_HIFI: &str = "map-hifi\0";
+pub static ASM: &str = "asm\0";
+pub static SHORT: &str = "short\0";
+pub static SR: &str = "sr\0";
+pub static SPLICE: &str = "splice\0";
+pub static CDNA: &str = "cdna\0";
 
-impl Default for mm_idxopt_t {
-    fn default() -> Self {
-        mm_idxopt_t {
-            k: 15,
-            w: 10,
-            flag: 0,
-            bucket_bits: 14,
-            mini_batch_size: 50000000,
-            batch_size: 4000000000,
+pub enum Preset {
+    MAP_ONT,
+    AVA_ONT,
+    MAP10K,
+    AVA_PB,
+    MAP_HIFI,
+    ASM,
+    SHORT,
+    SR,
+    SPLICE,
+    CDNA,
+}
+
+impl From<Preset> for *const i8 {
+    fn from(preset: Preset) -> Self {
+        match preset {
+            Preset::MAP_ONT => MAP_ONT.as_bytes().as_ptr() as *const i8,
+            Preset::AVA_ONT => AVA_ONT.as_bytes().as_ptr() as *const i8,
+            Preset::MAP10K => MAP10K.as_bytes().as_ptr() as *const i8,
+            Preset::AVA_PB => AVA_PB.as_bytes().as_ptr() as *const i8,
+            Preset::MAP_HIFI => MAP_HIFI.as_bytes().as_ptr() as *const i8,
+            Preset::ASM => ASM.as_bytes().as_ptr() as *const i8,
+            Preset::SHORT => SHORT.as_bytes().as_ptr() as *const i8,
+            Preset::SR => SR.as_bytes().as_ptr() as *const i8,
+            Preset::SPLICE => SPLICE.as_bytes().as_ptr() as *const i8,
+            Preset::CDNA => CDNA.as_bytes().as_ptr() as *const i8,
         }
     }
 }
 
-impl Default for mm_mapopt_t {
+#[derive(Debug, Clone)]
+pub struct Aligner {
+    idxopt: MaybeUninit<mm_idxopt_t>,
+    mapopt: MaybeUninit<mm_mapopt_t>,
+    idx: Option<MaybeUninit<*mut mm_idx_t>>,
+    idx_reader: MaybeUninit<*mut mm_idx_reader_t>,
+    threads: usize,
+    /* TODO: Goals for better ergonomics...
+
+    // mm_idx_opt
+    pub k: u16,
+    pub w: u16,
+    pub idxflag: u16,
+    pub bucket_bits: u16,
+    pub mini_batch_size_idx: i64, // Renamed from mini_batch_size
+    pub batch_size: u64,
+
+    // mapopt
+    pub mapflag: i64,
+    pub seed: i32,
+    pub sdust_threshold: i32, // Renamed from sdust_thres
+    pub max_qlen: i32,
+    pub bw: i32,
+    pub bw_long: i32,
+    pub max_gap: i32,
+    pub max_gap_ref: i32,
+    pub max_frag_len: i32,
+    pub max_chain_skip: i32,
+    pub max_chain_iter: i32,
+    pub min_cnt: i32,
+    pub min_chain_score: i32,
+    pub chain_gap_scale: f32,
+    pub chain_skip_scale: f32,
+    pub rmq_size_cap: i32,
+    pub rmq_inner_dist: i32,
+    pub rmq_rescue_size: i32,
+    pub rmq_rescue_ratio: f32,
+    pub mask_level: f32,
+    pub mask_len: i32,
+    pub pri_ratio: f32,
+    pub best_n: i32,
+    pub alt_drop: f32,
+    pub a: i32,
+    pub b: i32,
+    pub q: i32,
+    pub e: i32,
+    pub q2: i32,
+    pub e2: i32,
+    pub sc_ambi: i32,
+    pub noncan: i32,
+    pub junc_bonus: i32,
+    pub zdrop: i32,
+    pub zdrop_inv: i32,
+    pub end_bonus: i32,
+    pub min_dp_max: i32,
+    pub min_ksw_len: i32,
+    pub anchor_ext_len: i32,
+    pub anchor_ext_shift: i32,
+    pub max_clip_ratio: f32,
+    pub rank_min_len: i32,
+    pub rank_frac: f32,
+    pub pe_ori: i32,
+    pub pe_bonus: i32,
+    pub mid_occ_frac: f32,
+    pub q_occ_frac: f32,
+    pub min_mid_occ: i32,
+    pub max_mid_occ: i32,
+    pub mid_occ: i32,
+    pub max_occ: i32,
+    pub max_max_occ: i32,
+    pub occ_dist: i32,
+    pub mini_batch_size_map: i64, // Renamed from mini_batch_size
+    pub max_sw_mat: i64,
+    pub cap_kalloc: i64,
+    pub split_prefix: Vec<u8>,
+    */
+}
+
+impl Default for Aligner {
     fn default() -> Self {
-        mm_mapopt_t {
-            seed: 11,
-            mid_occ_frac: 2e-4,
-            min_mid_occ: 10,
-            max_mid_occ: 1_000_000,
-            sdust_thres: 0, // no SDUST masking
-            q_occ_frac: 0.01,
-            min_cnt: 3,
-            min_chain_score: 40,
-            bw: 500,
-            bw_long: 20_000,
-            max_gap: 5_000,
-            max_gap_ref: -1,
-            max_chain_skip: 25,
-            max_chain_iter: 5_000,
-            rmq_inner_dist: 1000,
-            rmq_size_cap: 100000,
-            rmq_rescue_size: 1000,
-            rmq_rescue_ratio: 0.1,
-            chain_gap_scale: 0.8,
-            chain_skip_scale: 0.0,
-            max_max_occ: 4095,
-            occ_dist: 500,
-            mask_level: 0.5,
-            mask_len: libc::INT_MAX,
-            pri_ratio: 0.8,
-            best_n: 5,
-            alt_drop: 0.15,
-            a: 2,
-            b: 4,
-            q: 4,
-            e: 2,
-            q2: 24,
-            e2: 1,
-            sc_ambi: 1,
-            zdrop: 400,
-            zdrop_inv: 200,
-            end_bonus: -1,
-            min_dp_max: 40 * 2, // min_chain_score * a
-            min_ksw_len: 200,
-            anchor_ext_len: 20,
-            anchor_ext_shift: 6,
-            max_clip_ratio: 1.0,
-            mini_batch_size: 500000000,
-            max_sw_mat: 100000000,
-            cap_kalloc: 1000000000,
-            rank_min_len: 500,
-            rank_frac: 0.9,
-            pe_ori: 0,
-            pe_bonus: 33,
+        let mut mm_idxopt = MaybeUninit::uninit();
+        let mut mm_mapopt = MaybeUninit::uninit();
 
-            // These values just grabbed randomly from the file...
-            flag: 0,
-            junc_bonus: 9, 
-            max_frag_len: 800,
-            max_qlen: 0,
-            noncan: 9,
-            mid_occ: todo!(),
-            max_occ: todo!(),
-            split_prefix: todo!(),
-            
-
+        unsafe { mm_set_opt(&0, mm_idxopt.as_mut_ptr(), mm_mapopt.as_mut_ptr()) };
+        Self {
+            idxopt: mm_idxopt,
+            mapopt: mm_mapopt,
+            threads: 1,
+            ..Default::default()
         }
     }
-} */
+}
 
+impl Aligner {
+    pub fn with_preset(preset: Preset) -> Self {
+        let mut mm_idxopt = MaybeUninit::uninit();
+        let mut mm_mapopt = MaybeUninit::uninit();
+
+        unsafe {
+            mm_set_opt(
+                preset.into(),
+                mm_idxopt.as_mut_ptr(),
+                mm_mapopt.as_mut_ptr(),
+            )
+        };
+        Self {
+            idxopt: mm_idxopt,
+            mapopt: mm_mapopt,
+            ..Default::default()
+        }
+    }
+
+    pub fn with_threads(mut self, threads: usize) -> Self {
+        self.threads = threads;
+        self
+    }
+
+    pub fn with_index() {
+        // Index, but instead pass output as None. Placeholder
+        todo!();
+    }
+
+    pub fn with_named_index(mut self, path: &Path, output: Option<&str>) -> Result<Self, &'static str> {
+        let path = match path.to_str() {
+            Some(path) => path,
+            None => return Err("Invalid path"),
+        };
+
+        let path = match std::ffi::CString::new(path) {
+            Ok(path) => path,
+            Err(_) => return Err("Invalid path"),
+        };
+
+        let output = match output {
+            Some(output) => match std::ffi::CString::new(output) {
+                Ok(output) => output,
+                Err(_) => return Err("Invalid output"),
+            },
+            None => std::ffi::CString::new(Vec::new()).unwrap(),
+        };
+
+        let idx_reader = MaybeUninit::new(unsafe {
+            mm_idx_reader_open(path.as_ptr(), &self.idxopt.assume_init(), output.as_ptr())
+        });
+
+        self.idx_reader = idx_reader;
+
+        let idx = MaybeUninit::new(unsafe {
+            mm_idx_reader_read(self.idx_reader.assume_init(), self.threads as libc::c_int)
+        });
+
+        self.idx = Some(idx);
+
+        Ok(self)
+    }
+
+    pub fn with_seq(mut self, seq: &[u8]) -> Result<Self, &'static str> {
+        let seq = match std::ffi::CString::new(seq) {
+            Ok(seq) => seq,
+            Err(_) => return Err("Invalid sequence"),
+        };
+
+        todo!();
+
+        //let idx = MaybeUninit::new(unsafe {
+            /*mm_idx_str(
+                self.idx_opt.w,
+                self.idx_opt.k,
+                self.idx_opt.flag & 1,
+                self.idx_opt.bucket_bits,
+                str.encode(seq),
+                len(seq),
+            )*/
+        //});
+
+        //self.idx = Some(idx);
+
+        Ok(self)
+    }
+
+    pub fn has_index(&self) -> bool {
+        self.idx.is_some()
+    }
+}
+
+impl Drop for Aligner {
+    fn drop(&mut self) {
+        if self.idx.is_some() {
+            unsafe { mm_idx_destroy(self.idx.unwrap().assume_init()) };
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::mem::MaybeUninit;
-    use libc::{c_char};
 
     #[test]
     fn does_it_work() {
         let mut mm_idxopt = MaybeUninit::uninit();
         let mut mm_mapopt = MaybeUninit::uninit();
 
-        unsafe { mm_set_opt(&PRESET0, mm_idxopt.as_mut_ptr(), mm_mapopt.as_mut_ptr()) };
+        unsafe { mm_set_opt(&0, mm_idxopt.as_mut_ptr(), mm_mapopt.as_mut_ptr()) };
         println!("{:#?}", unsafe { mm_idxopt.assume_init() });
         println!("{:#?}", unsafe { mm_mapopt.assume_init() }); // Run tests with --nocapture to see the output
-        
+    }
+
+    #[test]
+    fn create_index() {
+        Aligner::with_preset(Preset::MAP_ONT).with_threads(1).with_named_index(Path::new("test_data/test.fa"), Some("test.mmi")).unwrap();
     }
 }
