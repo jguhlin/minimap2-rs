e9538a36ee5adddaa340c747150cdc42d8bbac3c
jguhlin <joseph.guhlin@gmail.com>
2022-10-05 13:24:45 +1300

    Just some meddling

--- minimap2-sys/build.rs
+++ minimap2-sys/build.rs
@@ -4,18 +4,30 @@ use std::path::PathBuf;
 
 // #[cfg(feature = "bindgen")]
 fn gen() {
-    println!("Generating...");
+    let out_path = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    println!("{:#?}", out_path);
+
+    let host = env::var("HOST").unwrap();
+    let target = env::var("TARGET").unwrap();
+
     println!("cargo:rerun-if-changed=minimap2/*.c");
 
-    // kthread.o kalloc.o misc.o bseq.o sketch.o sdust.o options.o index.o
-    // lchain.o align.o hit.o seed.o map.o format.o pe.o esterr.o splitidx.o
-    // ksw2_ll_sse.o ksw2_extz2_sse41.o ksw2_extd2_sse41.o ksw2_exts2_sse41.o
-    // ksw2_extz2_sse2.o ksw2_extd2_sse2.o ksw2_exts2_sse2.o ksw2_dispatch.o
+    if !target.contains("msvc") && !target.contains("wasm") {
+        pkg_config::Config::new().probe("zlib").unwrap();
+    } else {
+        println!("cargo:rustc-link-lib=z");
+    }
+
+    println!("cargo:rustc-link-lib=m");
+    println!("cargo:rustc-link-lib=pthread");
 
     let mut cc = cc::Build::new();
+    cc.warnings(false);
+    cc.out_dir(&out_path);
+    cc.cpp_link_stdlib(None);
     cc.flag("-DHAVE_KALLOC");
     cc.flag("-O2");
-    cc.flag("-Wall");
+    //cc.flag("-Wall");
     cc.flag("-lm");
     cc.flag("-lz");
     cc.flag("-lpthread");
@@ -23,11 +35,15 @@ fn gen() {
     cc.flag("-DKSW_CPU_DISPATCH");
     cc.flag("-DKSW_SSE2_ONLY");
     cc.static_flag(true);
+    cc.include("minimap2");
 
     let files: Vec<_> = std::fs::read_dir("minimap2")
         .unwrap()
         .map(|f| f.unwrap().path())
         .collect();
+
+    assert!(files.len() != 0, "No files found in minimap2 directory -- Did you forget to clone the submodule? git submodule init --recursive");
+
     for file in files {
         // Skip "main.c" and "example.c"
         if file.file_name().unwrap() == "main.c" || file.file_name().unwrap() == "example.c" {
@@ -41,9 +57,11 @@ fn gen() {
         }
     }
 
+    println!("Compiling...");
+
     cc.compile("libminimap");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    println!("Compiled!");
 
     bindgen::Builder::default()
         .header("minimap2.h")
@@ -73,6 +91,7 @@ fn gen() {
         .allowlist_function("kseq_destroy")
         .allowlist_function("mm_map")
         .allowlist_function("mm_idx_str") */
+        .rustfmt_bindings(true)
         .generate()
         .expect("Couldn't write bindings!")
         .write_to_file(out_path.join("bindings.rs"))
@@ -83,8 +102,5 @@ fn gen() {
 // fn gen() {}
 
 fn main() {
-    pkg_config::Config::new().probe("zlib").unwrap();
-    println!("cargo:rustc-link-lib=m");
-    println!("cargo:rustc-link-lib=pthread");
     gen();
 }
