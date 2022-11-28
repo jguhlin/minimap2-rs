60d83033acab2be2ffea4f44d008342915097710
jguhlin <joseph.guhlin@gmail.com>
2022-11-29 10:12:25 +1300

    Working on musl support

--- minimap2-sys/build.rs
+++ minimap2-sys/build.rs
@@ -9,16 +9,18 @@ fn gen() {
     let out_path = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     println!("{:#?}", out_path);
 
-    let host = env::var("HOST").unwrap();
-    let target = env::var("TARGET").unwrap();
+    let _host = env::var("HOST").unwrap();
+    let _target = env::var("TARGET").unwrap();
 
     println!("cargo:rerun-if-changed=minimap2/*.c");
+    println!("cargo:rerun-if-env-changed=PKG_CONFIG_SYSROOT_DIR");
 
+    /*
     if !target.contains("msvc") && !target.contains("wasm") {
         pkg_config::Config::new().probe("zlib").unwrap();
-    } else {
+    } else if !target.contains("musl") {
         println!("cargo:rustc-link-lib=z");
-    }
+    } */
 
     println!("cargo:rustc-link-lib=m");
     println!("cargo:rustc-link-lib=pthread");
@@ -30,12 +32,23 @@ fn gen() {
     cc.flag("-DHAVE_KALLOC");
     cc.flag("-O2");
     cc.flag("-lm");
-    cc.flag("-lz");
+    // cc.flag("-lz");
     cc.flag("-lpthread");
     //cc.flag("-msse4.1");
     // cc.flag("-DKSW_CPU_DISPATCH");
     //cc.flag("-DKSW_SSE2_ONLY");
     cc.static_flag(true);
+
+    if let Some(include) = std::env::var_os("DEP_Z_INCLUDE") {
+        cc.include(include);
+    }
+
+    if let Ok(lib) = pkg_config::find_library("zlib") {
+        for path in &lib.include_paths {
+            cc.include(path);
+        }
+    }
+
     cc.include("minimap2");
 
     let files: Vec<_> = std::fs::read_dir("minimap2")
